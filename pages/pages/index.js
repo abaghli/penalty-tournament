10091d56cc36a0ce747f95c3da9c36b044b2ba83
abaghli <a.n.albaghli@gmail.com>
2025-03-26 16:55:38 +0300

    Delete pages/pages/index.js
    delete wrong index file

--- pages/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", padding: "50px" }}>
-      <h1>🎯 بطولة الأمير للركلات الترجيحية</h1>
-      <p>اضغط أدناه لعرض الفرق</p>
-      <a href="/teams" style={{ color: "blue", textDecoration: "underline" }}>عرض الفرق</a>
-    </div>
-  );
-}
